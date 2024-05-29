8dce2a3448ef0072399012fa7bb0b04677cf9680
NcUndstnd <Ignatcov169@gmail.com>
2024-05-29 11:19:58 +0300

    driver

--- Selenium/lab11/core/driver.js
+++ Selenium/lab11/core/driver.js
@@ -2,6 +2,10 @@ const { Builder } = require('selenium-webdriver')
 const chrome = require("selenium-webdriver/chrome");
 let options = new chrome.Options();
 
+options.addArguments('headless');
+options.addArguments('disable-gpu');
+options.addArguments('no-sandbox');
+options.addArguments('disable-dev-shm-usage');
 options.addArguments("user-data-dir=C:\\Users\\Ignat\\AppData\\Local\\Google\\Chrome\\User Data");
 options.addArguments("profile-directory=Default");
 
